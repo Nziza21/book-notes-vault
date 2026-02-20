b14a064b036a8d0115973f0b7420d0269dfd0236
Nziza21 <n.samuel@alustudent.com>
2026-02-20 11:24:30 +0200

    M3: add regex validator functions

---
+++ scripts/validators.js
@@ -0,0 +1,24 @@
+export function validateText(input) {
+  const re = /^\S(?:.*\S)?$/;
+  return re.test(input);
+}
+
+export function validatePages(input) {
+  const re = /^(0|[1-9]\d*)(\.\d{1,2})?$/;
+  return re.test(input);
+}
+
+export function validateDate(input) {
+  const re = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+  return re.test(input);
+}
+
+export function validateTag(input) {
+  const re = /^[A-Za-z]+(?:[ -][A-Za-z]+)*$/;
+  return re.test(input);
+}
+
+export function validateNoDuplicateWords(input) {
+  const re = /\b(\w+)\s+\1\b/;
+  return !re.test(input); 
+}
